75c4922aeb2cde79ed70a906ae91a90750c6978e
Peterson <Will.Peterson@turck.com>
2024-05-02 22:42:14 -0500

    First Commit

--- public/javascript/post-modal.js
+++ public/javascript/post-modal.js
@@ -1,23 +1,23 @@
 // Get the modal
-var modal = document.getElementById("post-modal");
+const postModal = document.getElementById("post-modal");
 
 // Get the button that opens the modal
-var modalTrigger = document.getElementById("post-modal-trigger");
+const postModalTrigger = document.getElementById("post-modal-trigger");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const postSpan = document.getElementsByClassName("post-span")[0];
 // When the user clicks on the button, open the modal
-modalTrigger.onclick = function () {
-  modal.style.display = "block";
+postModalTrigger.onclick = function () {
+  postModal.style.display = "block";
 };
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+postSpan.onclick = function () {
+  postModal.style.display = "none";
 };
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    postModal.style.display = "none";
   }
 };
